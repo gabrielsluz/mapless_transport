d194c3a0e5608be887a6a9801501da838998126a
gabrielsluz <gabrielsluz1999@gmail.com>
2023-09-11 15:20:42 -0300

    Laser rangefinder

--- research_envs/b2PushWorld/NavigationWorld.py
+++ research_envs/b2PushWorld/NavigationWorld.py
@@ -1,4 +1,7 @@
-from Box2D import b2World, b2ContactListener, b2QueryCallback, b2AABB, b2Vec2
+from Box2D import (
+    b2World, b2ContactListener, b2QueryCallback, 
+    b2RayCastCallback, b2AABB, b2Vec2, b2_pi
+)
 
 import numpy as np
 import cv2
@@ -38,6 +41,43 @@ class CheckOverlapCallback(b2QueryCallback):
 
     def reset(self):
         self.overlapped = False
+
+
+# ---------- Laser rangefinder ----------
+class LaserHit:
+    INVALID = 0
+    OBSTACLE = 1
+    OBJECT = 2
+    
+class RayCastClosestCallback(b2RayCastCallback):
+    """This callback finds the closest hit"""
+
+    def __repr__(self):
+        return 'Closest hit'
+
+    def __init__(self, **kwargs):
+        b2RayCastCallback.__init__(self, **kwargs)
+        self.fixture = None
+        self.hit = False
+
+    def ReportFixture(self, fixture, point, normal, fraction):
+        '''
+        Called for each fixture found in the query. You control how the ray
+        proceeds by returning a float that indicates the fractional length of
+        the ray. By returning 0, you set the ray length to zero. By returning
+        the current fraction, you proceed to find the closest point. By
+        returning 1, you continue with the original ray clipping. By returning
+        -1, you will filter out the current fixture (the ray will not hit it).
+        '''
+        self.hit = True
+        self.fixture = fixture
+        self.point = b2Vec2(point)
+        # self.normal = b2Vec2(normal)
+        # NOTE: You will get this error:
+        #   "TypeError: Swig director type mismatch in output value of
+        #    type 'float32'"
+        # without returning a value
+        return fraction
 
 
 class NavigationWorld:
@@ -80,8 +120,7 @@ class NavigationWorld:
         # ----------- Draw configuration -----------
         self.pixels_per_meter = 20
         self.screen_width = self.width * self.pixels_per_meter
-        self.screen_height = self.height * self.pixels_per_meter 
-        self.screen = np.zeros(shape=(self.screen_height, self.screen_width), dtype=np.float32)
+        self.screen_height = self.height * self.pixels_per_meter
 
         # Reset
         self.reset()
@@ -124,6 +163,39 @@ class NavigationWorld:
         self.goal.x = sampled_pos[0]
         self.goal.y = sampled_pos[1]
 
+    # ----------- Laser Sensor -----------
+    def get_laser_readings(self):
+        # Params:
+        ang_min = -b2_pi # start angle of the scan [rad]
+        ang_max = b2_pi # end angle of the scan [rad]
+        rays = 16 # indirect parameter, not used in the code
+        ang_inc = (ang_max - ang_min) / rays
+        range_max = 4.0 # maximum range of the sensor [m]
+
+        range_l = []
+        type_l = []
+        point_l = []
+        point1 = self.agent.agent_rigid_body.position
+        agent_ang = self.agent.agent_rigid_body.angle
+
+        ray_ang = ang_min
+        while ray_ang <= ang_max:
+            ang = ray_ang + agent_ang
+            ray_ang += ang_inc
+            point2 = point1 + range_max * b2Vec2(np.cos(ang), np.sin(ang))
+            callback = RayCastClosestCallback()
+            self.world.RayCast(callback, point1, point2)
+
+            if callback.hit:
+                range_l.append((point1 - callback.point).length)
+                type_l.append(LaserHit.OBSTACLE)
+                point_l.append(callback.point)
+            else:
+                range_l.append(-1)
+                type_l.append(LaserHit.INVALID)
+                point_l.append(point2)
+        return range_l, type_l, point_l
+
     # ----------- Draw functions -----------
 
     def worldToScreen(self, position):
@@ -131,17 +203,28 @@ class NavigationWorld:
 
     def drawToBuffer(self):
         # clear previous buffer
-        self.screen = np.ones(shape=(self.screen_height, self.screen_width, 3), dtype=np.float32)
+        screen = np.ones(shape=(self.screen_height, self.screen_width, 3), dtype=np.float32)
         # Draw obstacles
         for obs in self.obstacle_l:
-            obs.Draw(self.pixels_per_meter, self.screen, (0.5, 0.5, 0.5), -1)
+            obs.Draw(self.pixels_per_meter, screen, (0.5, 0.5, 0.5), -1)
         # Draw goal
         screen_pos = self.worldToScreen(self.goal)
-        cv2.circle(self.screen, screen_pos, int(self.goal_tolerance*self.pixels_per_meter), (0, 1, 0, 0), -1)
+        cv2.circle(screen, screen_pos, int(self.goal_tolerance*self.pixels_per_meter), (0, 1, 0, 0), -1)
         # Draw agent
         screen_pos = self.worldToScreen(self.agent.GetPositionAsList())
         if self.agent_collided == 0:
-            cv2.circle(self.screen, screen_pos, int(self.agent.agent_radius*self.pixels_per_meter), (1, 0, 0, 0), -1)
+            cv2.circle(screen, screen_pos, int(self.agent.agent_radius*self.pixels_per_meter), (1, 0, 0, 0), -1)
         else:
-            cv2.circle(self.screen, screen_pos, int(self.agent.agent_radius*self.pixels_per_meter), (0, 0, 1, 0), -1)
-        return self.screen
+            cv2.circle(screen, screen_pos, int(self.agent.agent_radius*self.pixels_per_meter), (0, 0, 1, 0), -1)
+        return screen
+
+    def drawToBufferWithLaser(self, laser_point_l):
+        screen = self.drawToBuffer()
+        # Draw laser
+        screen_pos = self.worldToScreen(self.agent.GetPositionAsList())
+        for point in laser_point_l:
+            # if point is not None:
+            screen_point = self.worldToScreen(point)
+            cv2.line(screen, screen_pos, screen_point, (0, 0, 1), 1)
+        return screen
+
--- research_envs/env_dev/navigation.py
+++ research_envs/env_dev/navigation.py
@@ -20,7 +20,8 @@ def key_to_action(key):
     return action
 
 def render():
-    scene_buffer.PushFrame(world.drawToBuffer())
+    range_l, type_l, point_l = world.get_laser_readings()
+    scene_buffer.PushFrame(world.drawToBufferWithLaser(point_l))
     scene_buffer.Draw()
     cv2.waitKey(1)
 
@@ -39,8 +40,10 @@ if __name__ == "__main__":
         action = key_to_action(key)
         if action != -1:
             # print('World update.')
+            # print(world.get_laser_readings())
             world.take_action(action)
             render()
+
             if world.did_agent_collide():
                 print('Agent collided with obstacle.')
                 world.reset()
